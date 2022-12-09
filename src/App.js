c246574d593ca05d7c8f430d0af9092f2a4361d2
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-12-09 16:48:37 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Launches from "./components/launches/Launches";
+import { useState } from "react";
 import { ReactQueryDevtools } from "react-query/devtools";
+import Navbar from "./components/Navbar";
+import People from "./components/People";
+import Planets from "./components/Planets";
 function App() {
+  const [page, setPage] = useState("planets")
   return (
     <div className="App">
       <div className="content">
-        <Launches />
+        <h1>React Query Demo</h1>
+        <Navbar/>
+        {(page === "planets")?(<Planets/>):(<People/>)}
       </div>
       <ReactQueryDevtools initialIsOpen={false} />
     </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <button>Planets</button>
+      <button>People</button>
+    </nav>
+  )
+}
+
+export default Navbar
